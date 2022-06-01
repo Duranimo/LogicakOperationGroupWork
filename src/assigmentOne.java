d3be619f1cd210db45d6ddd61fc7fdfb1066ad9b
Duranimo <cellatduran@gmail.com>
2022-06-01 21:17:58 +0100

    first commit

---
+++ src/assigmentOne.java
@@ -0,0 +1,17 @@
+
+public class assigmentOne {
+
+	public static void main(String[] args) {
+		// Write a program to swap 2 numbers without a temporary variable?
+
+	int a=10, b=20;
+	
+	System.out.println("Before the swap " +a+" "+b);
+	
+	a= a+b; // 10+20	a=30   	leaving b to be 20
+	b= a-b; // 30-20  	b=10	leaving a to be 30
+	a= a-b; // 30-10 	a=20 	leaving b to be 10
+	
+	System.out.println("After the swap " +a+" "+b);
+	
+}}
